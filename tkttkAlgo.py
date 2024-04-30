152203a2a35f8803a979d0e1e54735cbdb6f3ab3
Fer Sebas <io@fersebas.com>
2024-04-30 00:06:57 -0500

    Update algo

--- tkttkAlgo.py
+++ tkttkAlgo.py
@@ -2,7 +2,18 @@ import pandas as pd
 import re
 import openpyxl
 
-df = pd.read_csv('reservations.csv')
+df1 = pd.read_csv('reservations.csv')
+df2 = pd.read_csv('reservations_lodgify.csv')
+
+column_mapping = {
+    'DateArrival': 'Start date', 
+    'DateDeparture': 'End date',
+    'Name': 'Guest name',
+    'HouseName': 'Listing',
+    'RoomRatesTotal': 'Earnings'
+}
+
+df2.rename(columns=column_mapping, inplace=True)
 
 replacement_dict = {
     r'Beach Heaven[\s-]*Private Beach,?[\s]*Pool': 'Beach Heaven',
@@ -11,6 +22,8 @@ replacement_dict = {
     r'The Purple Pelican Inn/Private[\s]*Hot Tub': 'The Purple Pelican Inn',
     r'Bliss on the Bay![\s]*Pool&Boat Ramp': 'Bliss on the Bay',
     r'Barefoot Bungalow![\s]*\+[\s]*Beach/Pool': 'Barefoot Bungalow',
+    r'Dolphin Den-Pool-Boat Ramp-BBQ': 'Dolphin Den',
+    r'HotTub Hideaway! PoolTable& More': 'HotTub Hideaway'
 }
 
 cleaning_fee_dict = {
@@ -27,59 +40,96 @@ cleaning_fee_dict = {
     'Ocean Oasis': 0
 }
 
-df['Listing'] = df['Listing'].replace(replacement_dict, regex=True)
+# Replace values in 'Listing' column using replacement dictionary
+df1['Listing'] = df1['Listing'].replace(replacement_dict, regex=True)
+df2['Listing'] = df2['Listing'].replace(replacement_dict, regex=True)
 
-df['Start date'] = df['Start date'].str.extract(r'(\d{1,2}/\d{1,2}/\d{4})')
-df['End date'] = df['End date'].str.extract(r'(\d{1,2}/\d{1,2}/\d{4})')
+# Convert date columns to datetime format
+df1['Start date'] = pd.to_datetime(df1['Start date'], format='%m/%d/%Y')
+df1['End date'] = pd.to_datetime(df1['End date'], format='%m/%d/%Y')
 
-df['Start date'] = pd.to_datetime(df['Start date'], format='%m/%d/%Y')
-df['End date'] = pd.to_datetime(df['End date'], format='%m/%d/%Y')
-
-df = df.sort_values(by=['Listing', 'Start date'])
-
-df['Guest name'] = df['Guest name'] 
-
-df['Earnings'] = df['Earnings'].str.extract(r'(\d+\.\d+)').astype(float)
-
-df['Cleaning fee'] = df['Listing'].map(cleaning_fee_dict)
-
-df['Tourist tax'] = df['Earnings'] * 0.05
-
-df['Management fee'] = (df['Earnings'] - df['Cleaning fee']) * 0.18
+# Convert date columns from the second file to MM/DD/YYYY format
+df2['Start date'] = pd.to_datetime(df2['Start date'], format='%Y-%m-%d', errors= 'coerce')
+df2['End date'] = pd.to_datetime(df2['End date'], format='%Y-%m-%d', errors= 'coerce')
 
 
+# Sort DataFrame by 'Listing' and 'Start date'
+df1 = df1.sort_values(by=['Listing', 'Start date'])
+
+# Extract numeric values from 'Earnings' column and convert to float
+df1['Earnings'] = df1['Earnings'].str.replace(r'[^\d.]', '', regex=True).astype(float)
+
+# Map 'Cleaning fee' using cleaning fee dictionary
+df1['Cleaning fee'] = df1['Listing'].map(cleaning_fee_dict)
+df2['Cleaning fee'] = df2['Listing'].map(cleaning_fee_dict)
+
+# Calculate 'Tourist tax' and 'Management fee'
+df1['Tourist tax'] = df1['Earnings'] * 0.05
+df1['Total after fees'] = df1['Earnings'] - (df1['Cleaning fee'] + df1['Tourist tax'])
+df1['Management fee'] = df1['Total after fees'] * 0.18
+
+df2['Tourist tax'] = df2['Earnings'] * 0.05
+df2['Total after fees'] = df2['Earnings'] - (df2['Cleaning fee'] + df2['Tourist tax'])
+df2['Management fee'] = df2['Total after fees'] * 0.18
+
+# Add 'Platform' column and set all values to 'airbnb'
+df1['Platform'] = 'Airbnb'
+
+df2['Platform'] = df2['Source']
+
+# Extract month and year from selected_month and selected_year
 selected_month = 4
 selected_year = 2024
-df = df[(df['Start date'].dt.month == selected_month) & (df['Start date'].dt.year == selected_year)]
 
-df['Earnings'] = df['Earnings'].apply(lambda x: "${:,.2f}".format(x))
-df['Cleaning fee'] = df['Cleaning fee'].apply(lambda x: "${:,.2f}".format(x))
-df['Tourist tax'] = df['Tourist tax'].apply(lambda x: "${:,.2f}".format(x))
-df['Management fee'] = df['Management fee'].apply(lambda x: "${:,.2f}".format(x))
+# Filter data based on selected month and year
+df1 = df1[(df1['Start date'].dt.month == selected_month) & (df1['Start date'].dt.year == selected_year)]
+df2 = df2[(df2['Start date'].dt.month == selected_month) & (df2['Start date'].dt.year == selected_year)]
+
+# Format monetary columns
+df1['Earnings'] = df1['Earnings'].apply(lambda x: "${:,.2f}".format(x))
+df1['Cleaning fee'] = df1['Cleaning fee'].apply(lambda x: "${:,.2f}".format(x))
+df1['Tourist tax'] = df1['Tourist tax'].apply(lambda x: "${:,.2f}".format(x))
+df1['Management fee'] = df1['Management fee'].apply(lambda x: "${:,.2f}".format(x))
+
+df2['Earnings'] = df2['Earnings'].apply(lambda x: "${:,.2f}".format(x))
+df2['Cleaning fee'] = df2['Cleaning fee'].apply(lambda x: "${:,.2f}".format(x))
+df2['Tourist tax'] = df2['Tourist tax'].apply(lambda x: "${:,.2f}".format(x))
+df2['Management fee'] = df2['Management fee'].apply(lambda x: "${:,.2f}".format(x))
+
+df2 = df2[df2['Type'] == 'Booking']
+
+# Select columns for the final DataFrame
+df1 = df1[['Listing', 'Guest name', 'Start date', 'End date', 'Earnings', 'Cleaning fee', 'Tourist tax', 'Total after fees', 'Management fee', 'Platform']]
+df2 = df2[['Listing', 'Guest name', 'Start date', 'End date', 'Earnings', 'Cleaning fee', 'Tourist tax', 'Total after fees', 'Management fee',  'Platform']]
+
+# Add 'Platform' column and map values from 'source' column
+
+df2 = df2[df2['Platform'] != 'Airbnb']
 
 
-df = df[['Listing', 'Guest name', 'Start date', 'End date',  'Earnings', 'Cleaning fee', 'Tourist tax', 'Management fee']]
+# Merge data from df1 and df2
+merged_df = pd.concat([df1, df2], ignore_index=True)
 
+# Select only the desired columns
+merged_df = merged_df[['Listing', 'Guest name', 'Start date', 'End date', 'Earnings', 'Cleaning fee', 'Tourist tax', 'Total after fees', 'Management fee', 'Platform']]
+unique_listings = merged_df['Listing'].unique()
+
+# Write each listing to a separate sheet
 with pd.ExcelWriter(f'TKTTK-A-{selected_month}-{selected_year}.xlsx', engine='openpyxl') as writer:
     
-    df.to_excel(writer, sheet_name='Global', index=False)
+    merged_df.to_excel(writer, sheet_name='Global', index=False)
 
     workbook = writer.book
     worksheet = writer.sheets['Global']
 
     for col in range(1, 11):
-        
         col_letter = openpyxl.utils.get_column_letter(col)
         worksheet.column_dimensions[col_letter].width = 20
     
-    for listing_id in df['Listing'].unique():
-        
-        df_listing = df[df['Listing'] == listing_id]
-        
+    for listing_id in merged_df['Listing'].unique():
+        df_listing = merged_df[merged_df['Listing'] == listing_id]
         sanitized_sheet_name = re.sub(r'[\\/*?:\[\]]', '', f'{listing_id}')
-        
         df_listing.to_excel(writer, sheet_name=sanitized_sheet_name, index=False)
-
         worksheet = writer.sheets[sanitized_sheet_name]
         for col in range(1, 11):
             col_letter = openpyxl.utils.get_column_letter(col)
